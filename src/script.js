a8788fa68fa35309832baa12a8f4171909637d1c
LaurieBiguet <biguet.laurie@live.fr>
2023-01-17 17:02:41 +0100

    background

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const para = document.createElement("div");
+para.setAttribute("style", "background-color:black; width: 100%; height: 100%; position: absolute; z-index: 1000000000000000000");
+document.body.appendChild(para);
+console.log(document.body.appendChild(para));
+
+alert("Attention !");
+
+
+
+
+
+
