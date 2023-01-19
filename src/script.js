88284020921899e36513bea221be8d7161a76121
MônicaS <msalmazotech@gmail.com>
2023-01-19 14:28:43 +0100

    updatemonica

--- src/script.js
+++ src/script.js
@@ -1,14 +1,37 @@
-document.body.innerHTML = "<body></body>";
-
-alert("Attention !");
-
+//let blockSite = (document.body.innerHTML = "<body></body>");
+//let timerON = ();
 
 
-    
+// alert("Attention !");
+
+let interdits = [
+    "https://www.youporn.fr/",
+    "https://www.xvideos.com/",
+    "https://coolors.co/",
+    "https://www.lovelace.adatechschool.fr/portal/",
+];
+
+for (let i = 0; i< interdits.length; i++) {
+    console.log(interdits[i]);
+};
 
 
 
+//boucle pour parcourir le tableau//
+//console.log//
+
+// localStorage.setItem("interdits", JSON.stringify(interdits));
+
+// let timerSite = [
+//     "https://fr-fr.facebook.com/"
+// ];
+// localStorage.setItem("timerSite", JSON.stringify(timerSite));
 
 
-
-
+    // if (interdits) {
+               
+    //     console.log(interdits.includes());
+    // };
+    // elseif (timerSite) {
+    //     console.log ()
+    // };
