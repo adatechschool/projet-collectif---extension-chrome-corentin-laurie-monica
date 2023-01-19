2ccf629c6ba90765b7fca5454bdedec9a382081f
LaurieBiguet <biguet.laurie@live.fr>
2023-01-19 12:02:45 +0100

    alerte

--- src/script.js
+++ src/script.js
@@ -1,6 +1,17 @@
-document.body.innerHTML = "<body></body>";
+function blackBackground() {
+    document.body.innerHTML = "<body></body>";
+    document.body.setAttribute("id", "color");
 
-alert("Attention !");
+    setTimeout(() => {
+        confirm("Attention le contenu de cette page est susceptible d'être inapproprié, souhaitez-vous continuer ?");
+    }, "1000");
+
+
+};
+
+blackBackground();
+
+
 
 
 
