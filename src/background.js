4bae20aa850df637c1f333a9a382750160dd21a8
Corentin MONVILLERS <c.monvillers@live.fr>
2023-01-19 14:15:41 +0100

    boutton actif

--- src/background.js
+++ src/background.js
@@ -1,1 +1,10 @@
 console.log("Hello world")
+
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+//     if(changeInfo.status == "complete"){
+//       chrome.scripting.executeScript({
+//         files: ["script.js"],
+//         target: {tabId: tab.id}
+//       })
+//     }
+//   });
--- src/popup.js
+++ src/popup.js
@@ -1,2 +1,25 @@
 console.log("This is a popup!")
 
+
+document.addEventListener('DOMContentLoaded', function (){  
+    //on génère la quote au click du bouton
+    document.querySelector("#red").addEventListener("click", onclick, false) 
+    function onclick(){
+        // document.body.innerHTML = "<body></body>";
+
+        alert("Attention !");
+        //au click du bouton 'yes please' on load les quotes
+        //fetchData();
+    };
+
+}, false);
+// function stop(){
+   
+//     document.getElementById("red").click = alert("Ca marche !!!")
+//        boutonElement.addEventListener("click",stop)
+// }
+
+
+
+
+// stop(); 
