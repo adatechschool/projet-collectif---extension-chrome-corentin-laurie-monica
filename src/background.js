344a6907588892c704cfb9eb489522fa0af98a6b
MônicaS <msalmazotech@gmail.com>
2023-01-19 14:45:05 +0100

    versionstable

--- src/background.js
+++ src/background.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    if(changeInfo.status == 'complete'){
-      chrome.scripting.executeScript({
-        files: ['script.js'],
-        target: {tabId: tab.id}
-      })
-    }
-  });
-=======
-console.log("Hello world")
+console.log("Hello world");
 
 // chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 //     if(changeInfo.status == "complete"){
@@ -18,4 +8,3 @@ console.log("Hello world")
 //       })
 //     }
 //   });
->>>>>>> cef5c95d416c7c257edd79df79f8e3b6ac56e5d3
--- src/script.js
+++ src/script.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-//let blockSite = (document.body.innerHTML = "<body></body>");
-//let timerON = ();
-=======
 function blackBackground() {
     document.body.innerHTML = "<body></body>";
     document.body.setAttribute("id", "color");
@@ -20,7 +16,6 @@ blackBackground();
 
 
 
->>>>>>> cef5c95d416c7c257edd79df79f8e3b6ac56e5d3
 
 
 // alert("Attention !");
