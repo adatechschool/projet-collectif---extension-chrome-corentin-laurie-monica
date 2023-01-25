a08bb71ca8d9cfd8d96c44f5672711affa75a8fd
LaurieBiguet <biguet.laurie@live.fr>
2023-01-25 16:05:13 +0100

    popup

--- src/popup.js
+++ src/popup.js
@@ -2,18 +2,18 @@ document.addEventListener('DOMContentLoaded', function (){
   //on écoute le bouton pour créer l'objet
   document.querySelector('#red').addEventListener('click', onclick) 
   function onclick(){
-    alert("Blocage du site")
+    alert("Vous allez bloquer ce site !");
       //au click du bouton, on créé l'objet dans le storage 
       //pour pouvoir écouter le changement dans le script.js
-      chrome.storage.local.set({ objVerif : false})
+      chrome.storage.local.set({ objVerif : false});
   };
 
   document.querySelector('#red1').addEventListener('click', declick) 
   function declick(){
-    alert("Vous allez débloquer le site")
+    alert("Vous allez débloquer le site !");
       //au click du bouton, on créé l'objet dans le storage 
       //pour pouvoir écouter le changement dans le script.js
-      chrome.storage.local.set({ objVerif : true})
+      chrome.storage.local.set({ objVerif : true});
   };
 });
 
